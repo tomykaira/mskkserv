db7aeef607d72a49d2ef52909d521a8f9f9fa636
tomykaira <tomykaira@gmail.com>
2014-11-01 16:28:21 +0900

    Complete basic server feature

--- mskkserv.go
+++ mskkserv.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"log"
 	"net"
+	"strconv"
 
 	"github.com/tomykaira/mskkserv/skkserv"
 )
@@ -11,17 +12,22 @@ import (
 // TODO: configure addr via options
 const (
 	SERVER_TYPE = "tcp"
-	SERVER_ADDR = "localhost:1178"
-	SERVER_PORT = "1178" // skkserv
 )
 
+type Config struct {
+	host string
+	port int
+}
+
 func main() {
-	listener, err := net.Listen(SERVER_TYPE, SERVER_ADDR)
+	config := parseFlags()
+	addr := config.host + ":" + strconv.Itoa(config.port)
+	listener, err := net.Listen(SERVER_TYPE, addr)
 	if err != nil {
 		log.Fatalln("Error listening: ", err.Error())
 	}
 	defer listener.Close()
-	log.Println("Listening on " + SERVER_ADDR)
+	log.Println("Listening on " + addr)
 
 	for {
 		conn, err := listener.Accept()
@@ -29,10 +35,14 @@ func main() {
 			log.Println("Error accepting connection: ", err.Error())
 			continue
 		}
+		log.Println("Accepting connection from " + conn.RemoteAddr().String())
 		go skkserv.HandleRequest(conn)
 	}
 }
 
-func parseFlags() {
-
+func parseFlags() (config Config) {
+	flag.StringVar(&config.host, "host", "127.0.0.1", "Host to bind")
+	flag.IntVar(&config.port, "port", 1178, "Listening port")
+	flag.Parse()
+	return config
 }
--- skkserv/skkserv.go
+++ skkserv/skkserv.go
@@ -21,7 +21,7 @@ const (
 func HandleRequest(conn net.Conn) {
 	var buffer bytes.Buffer
 	for {
-		err := readFromConnection(conn, buffer)
+		err := readFromConnection(conn, &buffer)
 		if err != nil {
 			if err != io.EOF {
 				log.Println("Error reading from conn", err.Error())
@@ -30,18 +30,19 @@ func HandleRequest(conn net.Conn) {
 			return
 		}
 
-		toClose, err := processCommand(conn, buffer)
+		toClose, err := processCommand(conn, &buffer)
 		if err != nil {
 			log.Println("Unexpected error processing command", err.Error())
 		}
 		if toClose {
 			conn.Close()
+			log.Println("Closed conenction from " + conn.RemoteAddr().String())
 			return
 		}
 	}
 }
 
-func readFromConnection(conn net.Conn, buffer bytes.Buffer) (err error) {
+func readFromConnection(conn net.Conn, buffer *bytes.Buffer) (err error) {
 	const TEMP_BUFFER_LEN = 1024
 	readbuf := make([]byte, TEMP_BUFFER_LEN)
 	for {
@@ -49,7 +50,7 @@ func readFromConnection(conn net.Conn, buffer bytes.Buffer) (err error) {
 		if err != nil {
 			return err
 		}
-		buffer.Write(readbuf)
+		buffer.Write(readbuf[0:len])
 		if len < TEMP_BUFFER_LEN || readbuf[len-1] == '\n' {
 			break
 		}
@@ -57,28 +58,30 @@ func readFromConnection(conn net.Conn, buffer bytes.Buffer) (err error) {
 	return nil
 }
 
-func processCommand(conn net.Conn, buffer bytes.Buffer) (toClose bool, err error) {
-	command, err := buffer.ReadBytes('\n')
-	if err == io.EOF {
-		// command does not completed. wait for next chunk
-		buffer.Write(command)
-		return false, nil
-	} else if err != nil {
-		return true, err
-	}
+func processCommand(conn net.Conn, buffer *bytes.Buffer) (toClose bool, err error) {
+	for buffer.Len() > 0 {
+		command, err := buffer.ReadBytes('\n')
+		if err == io.EOF {
+			// command does not completed. wait for next chunk
+			buffer.Write(command)
+			return false, nil
+		} else if err != nil {
+			return true, err
+		}
 
-	switch command[0] {
-	case COMMAND_END:
-		return true, nil
-	case COMMAND_REQUEST:
-		writer := transform.NewWriter(conn, japanese.EUCJP.NewDecoder())
-		io.WriteString(writer, "1/あ/い/う/\n")
-	case COMMAND_VERSION:
-		conn.Write([]byte("mskkserv " + VERSION + " "))
-	case COMMAND_HOST:
-		conn.Write([]byte(conn.LocalAddr().String() + " "))
-	default:
-		return true, errors.New("Unknown command")
+		switch command[0] {
+		case COMMAND_END:
+			return true, nil
+		case COMMAND_REQUEST:
+			writer := transform.NewWriter(conn, japanese.EUCJP.NewEncoder())
+			io.WriteString(writer, "1/あ/い/う/\n")
+		case COMMAND_VERSION:
+			conn.Write([]byte("mskkserv " + VERSION + " \n"))
+		case COMMAND_HOST:
+			conn.Write([]byte(conn.LocalAddr().String() + " \n"))
+		default:
+			return true, errors.New("Unknown command")
+		}
 	}
 
 	return false, nil
